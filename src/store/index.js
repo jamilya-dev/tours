503c8d7abb68cbeab9af546b35fbfb45422eebeb
jamilya-dev <omg.groups@yandex.ru>
2024-10-09 18:33:46 +0300

    задание 9

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import tours from './tours';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  modules: {
+    tours,
+  },
+});
+export default store;
---
+++ src/views/HomeViews.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="wrapper mt-5">
+    <div class="container">
+      <div class="grid d-flex justify-content-between flex-wrap">
+        <router-link
+          :to="`/tours/${tour.id}`"
+          v-for="tour in tours"
+          :key="tour.id"
+        >
+          <div class="item-wrapper d-flex flex-column mb-4">
+            <img
+              :src="require(`@/assets/img/${tour.img}`)"
+              class="mb-3"
+              :alt="tour.img"
+            />
+
+            <h3 class="">{{ tour.title }}</h3>
+            <div class="description">
+              {{ tour.description }}
+            </div>
+          </div>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    tours() {
+      return this.$store.getters["getTours"];
+    },
+  },
+};
+</script>
